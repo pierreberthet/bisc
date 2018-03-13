b2a1d9e998f979eb16378a180ac9d9da9d4b036e
pierreberthet <berthetp@gmail.com>
2018-03-13 14:10:53 +0100

    compilation script tests for the BBP models. Python version about to work!

--- compile_epfl_models.py
+++ compile_epfl_models.py
@@ -3,15 +3,37 @@ from glob import glob
 
 CWD = os.getcwd()
 
-neurons = glob(os.path.join('morphologies/hoc_combos_syn.1_0_10.allzips', '*'))
-mech = 'mechanisms'
-for i, NRN in enumerate(neurons):
-    os.chdir(CWD)
-    os.chdir(NRN)
-    os.chdir(mech)
-    if os.path.isdir('x86_64'):
-        os.system('rm -r x86_64')
-    os.system('nrnivmodl')
-    print("compiled {} out of {}".format(i + 1, len(neurons)))
+NMODL = 'morphologies/hoc_combos_syn.1_0_10.allmods'
 
+neurons = glob(os.path.join('morphologies/hoc_combos_syn.1_0_10.allzips', 'L1*'))
+mech = 'mechanisms'
+
+if not os.path.isdir(NMODL):
+    os.mkdir(NMODL)
+i = 0
+for NRN in neurons:
+    for nmodl in glob(os.path.join(NRN, 'mechanisms', '*.mod')):
+        while not os.path.isfile(os.path.join(NMODL,
+                                 os.path.split(nmodl)[-1])):
+            os.system('cp {} {}'.format(nmodl,
+                                        os.path.join(NMODL, '.')))
+    print("copypasted {} out of {}".format(i + 1, len(neurons)))
+    i += 1
+os.chdir(NMODL)
+os.system('nrnivmodl')
 os.chdir(CWD)
+
+# neuron.load_mechanisms(NMODL)
+
+
+
+# for i, NRN in enumerate(neurons):
+#     os.chdir(CWD)
+#     os.chdir(NRN)
+#     os.chdir(mech)
+#     if os.path.isdir('x86_64'):
+#         os.system('rm -r x86_64')
+#     os.system('nrnivmodl')
+#     print("compiled {} out of {}".format(i + 1, len(neurons)))
+
+# os.chdir(CWD)
